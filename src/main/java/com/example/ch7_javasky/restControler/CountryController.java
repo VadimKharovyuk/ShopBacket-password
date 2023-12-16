a8cb6562201b73b6d50b9e69f3de3b34ecb82948
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2023-12-16 17:40:28 +0530

    add Country

--- src/main/java/com/example/ch7_javasky/restControler/CountryController.java
+++ src/main/java/com/example/ch7_javasky/restControler/CountryController.java
@@ -1,19 +1,42 @@
 package com.example.ch7_javasky.restControler;
 
 import com.example.ch7_javasky.modelDTO.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @RestController
 public class CountryController {
-    @GetMapping("/france")
-    public Country france(){
-        Country country =Country.of("France",67);
+//    @GetMapping("/france")
+//    public Country france(){
+//        Country country =Country.of("France",67);
+//        return country;
+//    }
+    @GetMapping("/italy")
+    public Country italy(){
+        Country country =Country.of("Italy",33);
         return country;
     }
+    @GetMapping("/france")
+    public ResponseEntity <Country> paris(){
+        Country c =Country.of("france",67);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED).
+                header("capital","Paris")
+                .header("favorite_food","chisse and vine")
+                .header("continent","Europe")
+                .body(c) ;
+
+
+    }
+
+
     @GetMapping("/all")
     public List<Country> all(){
         Country country1 =Country.of("China",67);
